d91e390926b22275019c40855882dc569a156509
DavidePeloso-dev <davide.peloso.e@gmail.com>
2024-01-27 16:02:50 +0100

    Add list, input and add function

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,87 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      tasks: [
+        {
+          text: "learn HTML",
+          done: true,
+
+        },
+        {
+          text: "learn JS",
+          done: true,
+
+        },
+        {
+          text: "learn Vue",
+          done: false,
+
+        },
+      ],
+      newTask: "",
+      error: false,
+    }
+
+  },
+  methods: {
+    deleteTask(i) {
+      this.tasks.splice(i, 1);
+    },
+    addTask() {
+      if (this.newTask != "") {
+        const newTaskEl = { text: this.newTask, done: false, };
+        let included;
+        this.tasks.forEach(task => {
+          if (task.text == newTaskEl.text) {
+            included = true;
+          };
+        });
+        if (!included) {
+          this.error = false;
+          this.tasks.push(newTaskEl);
+          this.newTask = "";
+          console.log(this.tasks);
+        } else { this.error = "Your task is already in list" };
+      } else { this.error = "You have to type something!" };
+    },
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="text-center">
+    <h1>Todo List</h1>
+
+    <input v-model="newTask" @keyup.enter="addTask" type="text" name="newTask" id="newTask"
+      placeholder="Insert your new Task">
+    <button @click="addTask">Add</button>
+
+    <p class="text-danger" v-if="error">{{ error }}</p>
+
+    <ul>
+      <li v-for="( task, index ) in  tasks ">
+        <span :class="{ done: task.done }">{{ task.text }}</span>
+        <span @click="deleteTask(index)"> x</span>
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+/*#region reset */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style: none;
+  text-decoration: none;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+/* #endregion */
+
+.done {
+  text-decoration: line-through;
 }
 </style>
